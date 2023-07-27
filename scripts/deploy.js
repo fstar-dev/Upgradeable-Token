f4a205a0181192d78c751ba8d63e0730b82d50b7
dev-jovanni <jovanni.millers123@outlook.com>
2023-07-27 02:59:42 -0700

    remove swap code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,27 @@
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const TestToken1 = await ethers.getContractFactory(
+    "TestToken1"
+  );
+  console.log("Deploying TestToken1...");
+  const contract = await upgrades.deployProxy(TestToken1, [], {
+    initializer: "initialize",
+    kind: "transparent",
+  });
+  await contract.deployed();
+  console.log("TestToken1 deployed to:", contract.address);
 
-  // We get the contract to deploy
-  const TestToken = await hre.ethers.getContractFactory("testToken");
-  const testToken = await TestToken.deploy("100000000000000000000");
-
-  await testToken.deployed();
-  console.log("testToken deployed to:", testToken.address);
+  const TestToken2 = await ethers.getContractFactory(
+    "TestToken2"
+  );
+  console.log("Deploying TestToken1...");
+  const contract2 = await upgrades.deployProxy(TestToken2, [], {
+    initializer: "initialize",
+    kind: "transparent",
+  });
+  await contract2.deployed();
+  console.log("TestToken2 deployed to:", contract2.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main();
